71a1d414f68d27ba84dcc42558e67d9ada42f7df
asylbekuly <rabatkarabek8@gmail.com>
2024-12-28 22:26:02 +0500

    changed web.js, but error is "invalid incode" and "web3 is not a constructor"

--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   networks: {
       development: {
-          host: "127.0.0.1",  // Адрес Ganache
-          port: 7545,         // Порт Ganache
-          network_id: "5777", // NETWORK ID Ganache
-          gas: 8000000, // Увеличьте лимит газа
-          gasPrice: 20000000000, // Цена газа (Wei)
+          host: "127.0.0.1",  
+          port: 7545,         
+          network_id: "5777", 
+          gas: 9500000,
+          gasPrice: 20000000000, 
       },
   },
   compilers: {
       solc: {
-          version: "^0.8.0", // Версия Solidity
+          version: "^0.8.0", 
       },
   },
 };
--- web3.js
+++ web3.js
@@ -1,14 +1,74 @@
-const Web3 = require('web3');
-const fs = require('fs');
-const path = require('path');
+const Web3 = require("web3");
 
-// Connect to Ganache
-const web3 = new Web3('http://127.0.0.1:7545');
+// Подключение к Ganache
+const web3 = new Web3("http://127.0.0.1:7545");
 
-// Load ABI and Contract Address
-const abi = JSON.parse(fs.readFileSync(path.resolve(__dirname, './EtherWalletABI.json'), 'utf-8'));
-const contractAddress = '0x238D2f3da559A647f63Dc359776F90FDF7578f39';
+// ABI и адрес контракта
+const abi = [
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "stateMutability": "payable",
+        "type": "receive"
+    },
+    {
+        "inputs": [],
+        "name": "getBalance",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "withdrawAll",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
+const contractAddress = "0x238D2f3da559A647f63Dc359776F90FDF7578f39"; // Адрес контракта после развертывания
 
 const contract = new web3.eth.Contract(abi, contractAddress);
 
-module.exports = { web3, contract };
+async function interact() {
+    const accounts = await web3.eth.getAccounts();
+
+    // Проверить баланс контракта
+    const balance = await contract.methods.getBalance().call();
+    console.log("Баланс контракта:", web3.utils.fromWei(balance, "ether"), "ETH");
+
+    // Отправить Ether в контракт
+    await web3.eth.sendTransaction({
+        from: accounts[0],
+        to: contractAddress,
+        value: web3.utils.toWei("1", "ether"),
+    });
+
+    // Вывести средства
+    await contract.methods.withdrawAll().send({ from: accounts[0] });
+    console.log("Средства выведены!");
+}
+
+interact();
